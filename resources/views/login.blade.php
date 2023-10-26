d5df47e77f4156fb5676b945069a87eb07dd1774
jowy99 <jowycarreras@gmail.com>
2023-10-26 16:35:35 +0200

    Responsive

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,15 +2,13 @@
 
 @section('main')
 <div class="min-h-screen w-full mt-12 space-y-12">
-    <div class="flex flex-col items-center justify-center space-y-12">
-        <img src="https://i.pinimg.com/564x/da/b4/59/dab459576ce6988d79375b4a19d73534.jpg" alt="" class="w-10/12">
-        <div>
-            <div>
-                <h2 class="text-center text-orange-500 font-semibold text-xl">Plan your week, eat better: Your menu, your lifestyle!</h2>
-                <div class="grid grid-cols-2 gap-4">
-                    <x-button>register</x-button>
-                    <x-button variant="secondary">log in</x-button>
-                </div>
+    <div class="flex flex-col items-center justify-center space-y-12 mx-4 lg:flex">
+        <img src="https://i.pinimg.com/564x/da/b4/59/dab459576ce6988d79375b4a19d73534.jpg" alt="" class="w-10/12 lg:w-full lg:h-full">
+        <div class="space-y-12 lg:w-full lg:h-full">
+            <h2 class="text-center text-orange-500 font-semibold text-xl">Plan your week, eat better: Your menu, your lifestyle!</h2>
+            <div class="grid grid-cols-2 gap-4">
+                <x-button>register</x-button>
+                <x-button variant="secondary">log in</x-button>
             </div>
         </div>
     </div>
